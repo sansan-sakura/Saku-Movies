230f924f4aeee7e1b961ff4352fb42588e9a8b99
Sakura Tanaka <sito6496t@gmail.com>
2023-11-07 09:09:09 +0100

    test

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -11,38 +11,36 @@ function HeroCarousel() {
     <>
       <div className={styles.carousel_outer}>
         <Slider currentImageWidth={itemWidth} heroRef={heroRef} onchange={toggleActive}>
-          {movies.map((movie) => (
-            <>
-              <li key={movie.id} className={styles.hero_image_wrapper} ref={itemRef}>
-                <div className={styles.hero_img_inner_wrapper}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                    className={styles.hero_image}
-                  />
-                  <div className={styles.inner_image_box}>
-                    <div className={styles.inner_image_box_inner}>
-                      <img
-                        src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
-                        className={styles.poster_img}
-                      />
-                      <h2>{movie.title}</h2>
-                    </div>
-                  </div>
-                  <div className={styles.pagination_box}>
-                    {Array.from({ length: movies.length }, (_, i) => (
-                      <div
-                        onClick={() => handleToggleActive(i)}
-                        key={movies[i].id}
-                        className={styles.pagination}
-                        style={{
-                          backgroundColor: currentIndex === i ? "#fff" : "",
-                        }}
-                      ></div>
-                    ))}
+          {movies.map((movie, i) => (
+            <li key={movie.id} className={styles.hero_image_wrapper} ref={itemRef}>
+              <div className={styles.hero_img_inner_wrapper}>
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  className={styles.hero_image}
+                />
+                <div className={styles.inner_image_box}>
+                  <div className={styles.inner_image_box_inner}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                      className={styles.poster_img}
+                    />
+                    <h2>{movie.title}</h2>
                   </div>
                 </div>
-              </li>
-            </>
+                <div className={styles.pagination_box}>
+                  {Array.from({ length: movies.length }, (_, i) => (
+                    <div
+                      onClick={() => handleToggleActive(i)}
+                      key={movies[i].id}
+                      className={styles.pagination}
+                      style={{
+                        backgroundColor: currentIndex === i ? "#fff" : "",
+                      }}
+                    ></div>
+                  ))}
+                </div>
+              </div>
+            </li>
           ))}
         </Slider>
       </div>
--- src/hooks/useScreenSize.jsx
+++ src/hooks/useScreenSize.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 function getWindowWidth() {
   const { innerWidth: width } = window;
   return {
